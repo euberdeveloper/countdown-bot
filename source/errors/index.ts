14845a9ab75dbb4fe60635933af3b97c9dcb2194
euberdeveloper <euberdeveloper@gmail.com>
2023-10-30 18:11:41 +0100

    style: lint code
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/errors/index.ts
+++ source/errors/index.ts
@@ -6,28 +6,40 @@ export class CountdownBotError extends Error {
 }
 
 export class InvalidTimeError extends CountdownBotError {
-    constructor(public time: string, message = 'Invalid time: ' + time) {
+    constructor(
+        public time: string,
+        message = 'Invalid time: ' + time
+    ) {
         super(message);
         this.name = 'InvalidTimeError';
     }
 }
 
 export class TimeNotSpecifiedError extends InvalidTimeError {
-    constructor(public time: string, message = 'Time not specified: ' + time) {
+    constructor(
+        public time: string,
+        message = 'Time not specified: ' + time
+    ) {
         super(message, time);
         this.name = 'TimeNotSpecifiedError';
     }
 }
 
 export class TimeIsNaNError extends InvalidTimeError {
-    constructor(public time: string, message = 'Time is not a number: ' + time) {
+    constructor(
+        public time: string,
+        message = 'Time is not a number: ' + time
+    ) {
         super(message, time);
         this.name = 'TimeIsNaNError';
     }
 }
 
 export class TimeIsNegativeError extends InvalidTimeError {
-    constructor(public time: string, message = 'Time is negative: ' + time) {
+    constructor(
+        public time: string,
+        message = 'Time is negative: ' + time
+    ) {
         super(message, time);
         this.name = 'TimeIsNegativeError';
     }
