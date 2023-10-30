5339d013bb88bb6094abb0da5909ed7743c642fa
euberdeveloper <euberdeveloper@gmail.com>
2023-10-30 17:50:21 +0100

    refactor(commands): add property hidden
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/commands/types.ts
+++ source/commands/types.ts
@@ -4,6 +4,6 @@ import type { BotCommand } from 'grammy/types';
 import { CountDownContext } from '../types/index.js';
 
 export interface EuberBotCommand extends BotCommand {
-    command: string | null;
     handler: CommandMiddleware<CountDownContext>;
+    hidden?: boolean;
 }
--- source/commands/utils.ts
+++ source/commands/utils.ts
@@ -4,11 +4,11 @@ import { Bot } from 'grammy';
 
 export function getCommandsHelp(commands: EuberBotCommand[]): BotCommand[] {
     return commands
-        .filter(command => command.description !== null)
+        .filter(command => command.hidden)
         .map<BotCommand>(({ command, description }) => ({ command, description }));
 }
 
-export async function setCommands(bot: Bot, commands: EuberBotCommand[]) {
+export async function setCommands(bot: Bot, commands: EuberBotCommand[]): Promise<void> {
     const commandsHelp = getCommandsHelp(commands);
     await bot.api.setMyCommands(commandsHelp);
 }
